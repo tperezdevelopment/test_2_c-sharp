4f9fdb26eab32d8a77a5b50f148c28b702aa2701
tperezdevelopment <43383476+tperezdevelopment@users.noreply.github.com>
2023-12-11 20:49:40 -0500

    EventClass and Generic Class
    EventClass.css for the Event question and Generic Class for the generic question

---
+++ Test_2/EventClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+public class MyEventArguments : EventArgs
+{
+    public string Message { get; set; }
+
+    public MyEventArguments(string message)
+    {
+        Message = message;
+    }
+}
+
+public delegate void MyEventHandler(object sender, MyEventArguments e);
+
+public class ClassEvent
+{
+    public event MyEventHandler MyEvent;
+
+    public void ThroughEvent(string message)
+    {
+        MyEvent?.Invoke(this, new MyEventArguments(message));
+    }
+}
+
+public class MyManagerEvent
+{
+    public void ManagerEvent(object sender, MyEventArguments e)
+    {
+        Console.WriteLine($"The Manager event speaking {e.Message}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ClassEvent classEvent = new ClassEvent();
+        MyManagerEvent managerEvent = new MyManagerEvent();
+
+        classEvent.MyEvent += managerEvent.ManagerEvent;
+
+        classEvent.ThroughEvent("This is the message");
+
+
+
+    }
+}
+
+
+
---
+++ Test_2/GenericClass.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+
+public class Class1<T> where T : struct
+{
+    List<T> list = new List<T>();
+
+    public void Add(T item)
+    {
+        list.Add(item);
+    }
+
+    public T getElement(int index)
+    {
+        try
+        {
+            return list[index];
+        }
+        catch
+        {
+            throw new IndexOutOfRangeException($"Index {index} out of range");
+        }
+    }
+
+    public List<T> OrderCollection()
+    {
+        return list.OrderByDescending(item => item).ToList();
+    }
+}
